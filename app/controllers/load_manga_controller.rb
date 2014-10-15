08597a54f0738955c0d2c6b0361ae401e6d8f181
Iksha Gurung <grg.iksha@hotmail.com>
2014-10-15 09:23:15 +0545

    Remove binding.pry.

--- app/controllers/load_manga_controller.rb
+++ app/controllers/load_manga_controller.rb
@@ -1,5 +1,19 @@
 class LoadMangaController < ActionController::Base
+  layout 'application'
   def index
-    binding.pry
+    url = params['url']
+    if url
+      template = Phantomjs.run("#{Rails.root}/lib/phantom_renderer.js", url)
+      raw_html = Nokogiri::HTML.parse(template)
+      url_array = url.split('/')
+      @last_page = url_array.pop()
+      image_url = raw_html.css('div#imgholder a').first
+      @image_url = image_url.children.first.attributes['src'] if image_url
+      if url_array.length == 5
+        @next_page_url = (url_array << (@last_page.to_i + 1)).join('/')
+      elsif @last_page == @image_url.value.split('/')[-2].to_s
+        @next_page_url = url + '/' + '1'
+      end
+    end
   end
 end
